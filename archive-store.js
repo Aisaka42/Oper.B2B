c42e9c0b5551ed447406678ab8db1a6447e4f8a6
Aisaka42 <aisaka_@mail.ru>
2026-05-21 09:35:45 +0700

    Update weekly archive: project_protocol_18.05.2026_S-26-02_Партнер 2.0.md

--- archive-store.js
+++ archive-store.js
@@ -1,7 +1,21 @@
 window.archiveRepoData = {
   "site": "operational-projects-b2b",
-  "version": "2026-05-21T02:23:41.872Z",
+  "version": "2026-05-21T02:35:45.543Z",
   "archiveDocs": [
+    {
+      "id": "project_protocol_18.05.2026_S-26-02_Партнер 2.0.md",
+      "name": "project_protocol_18.05.2026_S-26-02_Партнер 2.0.md",
+      "sourceName": "project_protocol_18.05.2026_S-26-02_Партнер 2.0.md",
+      "type": "project_protocol",
+      "projectCode": "S-26-02",
+      "projectName": "Партнер 2.0",
+      "periodDate": "18.05.2026",
+      "ext": "md",
+      "size": 8584,
+      "mime": "text/markdown",
+      "savedAt": "2026-05-21T02:35:43.764Z",
+      "filePath": "./archive/weekly/project_protocol_18.05.2026_S-26-02_Партнер 2.0.md"
+    },
     {
       "id": "rating_04.05.2026_S-26-02_Партнер 2.0.md",
       "name": "rating_04.05.2026_S-26-02_Партнер 2.0.md",
