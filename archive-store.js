9d166df49e73510aca95df2c8f6c413da58adcf4
Aisaka42 <aisaka_@mail.ru>
2026-05-21 08:29:59 +0700

    Update charter snapshots for S 2026-05

--- archive-store.js
+++ archive-store.js
@@ -1,6 +1,6 @@
 window.archiveRepoData = {
   "site": "operational-projects-b2b",
-  "version": "2026-05-20T13:43:17.341Z",
+  "version": "2026-05-21T01:29:59.538Z",
   "archiveDocs": [
     {
       "id": "rating_04.05.2026_S-26-02_Партнер 2.0.md",
@@ -17,5 +17,55 @@ window.archiveRepoData = {
       "filePath": "./archive/weekly/rating_04.05.2026_S-26-02_Партнер 2.0.md"
     }
   ],
-  "snapshotRecords": []
+  "snapshotRecords": [
+    {
+      "id": "S_2026-05_v06_1779326998152",
+      "projectCode": "S",
+      "projectName": "Взрыв продаж B2O, 2 квартал 2026",
+      "snapshotMonth": "2026-05",
+      "charterVersion": "v06",
+      "charterDate": "2026-05-12",
+      "section": "6. План/факт",
+      "metrics": [
+        {
+          "name": "Выручка B2O, 2 квартал, руб. с НДС",
+          "comment": "",
+          "rows": [
+            {
+              "month": "04",
+              "plan": "618 525",
+              "fact": null,
+              "deviation": null
+            },
+            {
+              "month": "05",
+              "plan": "923 525",
+              "fact": null,
+              "deviation": null
+            },
+            {
+              "month": "06",
+              "plan": "1 173 525",
+              "fact": null,
+              "deviation": null
+            }
+          ]
+        }
+      ],
+      "warnings": [
+        "Код проекта не найден в шапке: использован код карточки проекта.",
+        "Для выбранного месяца есть незаполненный факт: Выручка B2O, 2 квартал, руб. с НДС."
+      ],
+      "errors": [],
+      "status": "needs-review",
+      "uploadedAt": "2026-05-21T01:29:58.151Z",
+      "uploadedBy": "долженко диана",
+      "comment": "",
+      "sourceFileName": "Устав 2Квартал.md",
+      "sourceMime": "text/markdown",
+      "sourceSize": 11693,
+      "sourceText": "",
+      "sourceFilePath": "./archive/charters/Устав 2Квартал.md"
+    }
+  ]
 };
